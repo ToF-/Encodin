43fe853b7042ce676d3b4d15797f640828ba228d
ToF <cthibaut@octo.com>
2017-11-05 11:39:30 +0100

    stage 1:output for a single sequence incorrectly prints count

--- C/encoding.c
+++ C/encoding.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { char *message = test(); tests_run++; \
                                 if (message) return message; } while (0)
 
 int tests_run = 0;
 
-int foo = 7;
-int bar = 5;
+static int foo = 7;
+static int bar = 5;
 
 static char *test_foo() {
     mu_assert("error, foo != 7", foo == 7);
@@ -24,7 +26,7 @@ static char *all_tests() {
     return 0;
 }
 
-int main(int argc, char **argv) {
+int run_all_tests() {
     char *result = all_tests();
     if (result != 0) {
         printf("%s\n", result);
@@ -33,6 +35,45 @@ int main(int argc, char **argv) {
         printf("ALL TESTS PASSED\n");
     }
     printf("Tests run: %d\n", tests_run);
-    return result != 0;
+    return result != 0; 
+}
+    
+int encode(char *srce, char *dest) {
+    char last = -1;
+    int  count = 0;
+    while(*srce != '\0') {
+        if (*srce == last) {
+            count++;
+        } else {
+            *dest++ = count + '0';
+            *dest++ = last;
+            last = *srce;
+        }
+        srce++;
+    }
+    *dest++ = '\0';
+    return 0;
+}
+
+void process() {
+    char *srce = NULL;
+    char *dest = NULL;
+    size_t len = 0;
+    size_t read = 0;
+    while(read != -1) {
+        read = getline(&srce, &len, stdin);
+        if (read == -1)
+            break;
+        dest = (char *)malloc(len); 
+        encode(srce, dest);
+        fputs(dest, stdout);
+        free(dest);
+    }   
+}
+
+int main(int argc, char **argv) {
+    process();
+    // return run_all_tests();
+    return 0;
 }
 
